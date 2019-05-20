787d5576862a3f18c4f7e018b840163a340f5157
Shaji James <shajijames7@gmail.com>
2019-05-20 13:01:55 +0530

    Update Column level check.py

--- Column level check.py
+++ Column level check.py
@@ -8,17 +8,21 @@ Created on Mon May  6 16:10:48 2019
 import pandas as pd
 import pyodbc
 
-con=pyodbc.connect('Driver={**********};'
-                'Server=************;'
-                'Database=********;'
-                'uid=**********;'
-                'pwd=**********')    
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+
+con=pyodbc.connect('Driver={****};'
+                'Server=****;'
+                'Database=****;'
+                'uid=****;'
+                'pwd=****')    
 cursor=con.cursor()
 
 src_d=  input('Enter source database: ')
 src_s=  input('Enter source schema: ')
-src_t = input('Enter source table/query: ')
-src_t=src_t.lstrip()
+src_t = input('Enter source table/query: ')  
+
+src_t=str.lower(src_t.lstrip())
 if src_t[0:3]=='sel':
     src_t='('+src_t+')'
 
@@ -58,8 +62,6 @@ while x=='Y' or x=='y':
     x=input('Do you want to add more key columns (y/n): ')
 else:
     print('Thanks')
-
-key_list[0][0]
   
 on=''
 for i in range(len(key_list)):
@@ -74,38 +76,70 @@ tgt_k=[]
 for i in range(len(key_list)):
     tgt_k.append(key_list[i][1])
 
-print('\nSelect columns to be matched:')
-print('\nSource columns:')
-for i in range(len(df_src_c['COLUMN_NAME'])):
-    if i not in src_k:
-        print(str(i+1) +' : '+ df_src_c['COLUMN_NAME'][i])
+ma_list=[]    
+au=input('\nDo you want to proceed with auto-match (y/n): ')
+if au=='Y' or au=='y':
+    for i in range(len(df_tgt_c['COLUMN_NAME'])):
+        if i not in tgt_k:
+            for j in range(len(df_src_c['COLUMN_NAME'])):
+                if i not in src_k:
+                    if str.lower(df_tgt_c['COLUMN_NAME'][i])==str.lower(df_src_c['COLUMN_NAME'][j]):
+                        ma_list.append([j,i])                        
+    for i in range(len(ma_list)):
+        print(str(i+1)+': '+df_tgt_c['COLUMN_NAME'][ma_list[i][1]]+' = '+df_src_c['COLUMN_NAME'][ma_list[i][0]])
+    
+    dr=input('\nDo you want to drop any match (y/n): ')
+    if dr=='Y' or dr=='y':
+        dr_s=input('\nEnter the matches to be dropped separated by comma: ')
+        dr_l=[int(i)-1 for i in dr_s.split(',')]
+        for i in sorted(dr_l, reverse=True):
+            del ma_list[i]
+else:
+    print('')
+        
+for i in range(len(ma_list)):
+    src_k.append(ma_list[i][0])
+for i in range(len(ma_list)):
+    tgt_k.append(ma_list[i][1])
 
-print('\nTarget columns:')
+print('\nColumns in target that are left to be matched:')    
 for i in range(len(df_tgt_c['COLUMN_NAME'])):
     if i not in tgt_k:
-        print(str(i+1) +' : '+ df_tgt_c['COLUMN_NAME'][i])
+        print('\t'+df_tgt_c['COLUMN_NAME'][i])
 
-match_list=[]
-x='Y'
-while x=='Y' or x=='y':
-    l=[]
-    l.append(int(input('Enter source column: '))-1)
-    l.append(int(input('Enter target column to match: '))-1)
-    match_list.append(l)
-    x=input('Do you want to add more columns (y/n): ')
-else:
-    print('Thanks')
+ma=input('\nDo you want to proceed with manual-match (y/n): ')
+if ma=='Y' or ma=='y':
+    print('\nSelect columns to be matched:')
+    print('\nSource columns:')
+    for i in range(len(df_src_c['COLUMN_NAME'])):
+        if i not in src_k:
+            print(str(i+1) +' : '+ df_src_c['COLUMN_NAME'][i])
+    
+    print('\nTarget columns:')
+    for i in range(len(df_tgt_c['COLUMN_NAME'])):
+        if i not in tgt_k:
+            print(str(i+1) +' : '+ df_tgt_c['COLUMN_NAME'][i])
+    
+    x='Y'
+    while x=='Y' or x=='y':
+        l=[]
+        l.append(int(input('Enter source column: '))-1)
+        l.append(int(input('Enter target column to match: '))-1)
+        ma_list.append(l)
+        x=input('Do you want to add more columns (y/n): ')
+    else:
+        print('Thanks')
     
     
-if src_t[1:4]=='sel':
+if str.lower(src_t[1:4])=='sel':
     q='select * from '+tgt_d+'.'+tgt_s+'.'+tgt_t +' a left join '+src_t+' b on '+on+' where '
-    for i in range(len(match_list)):
-        q+=' a.'+df_tgt_c['COLUMN_NAME'][match_list[i][0]]+' != b.'+df_tgt_c['COLUMN_NAME'][match_list[i][1]]+' or '
+    for i in range(len(ma_list)):
+        q+=' a.'+df_tgt_c['COLUMN_NAME'][ma_list[i][1]]+' != b.'+df_src_c['COLUMN_NAME'][ma_list[i][0]]+' or '
     q=q[:-4]
 else:
     q='select * from '+tgt_d+'.'+tgt_s+'.'+tgt_t +' a left join '+src_d+'.'+src_s+'.'+src_t+' b on '+on+' where '
-    for i in range(len(match_list)):
-        q+=' a.'+df_tgt_c['COLUMN_NAME'][match_list[i][0]]+' != b.'+df_tgt_c['COLUMN_NAME'][match_list[i][1]]+' or '
+    for i in range(len(ma_list)):
+        q+=' a.'+df_tgt_c['COLUMN_NAME'][ma_list[i][1]]+' != b.'+df_src_c['COLUMN_NAME'][ma_list[i][0]]+' or '
     q=q[:-4]
 
 df=pd.read_sql(q,con)
@@ -113,7 +147,26 @@ df=pd.read_sql(q,con)
 con.commit() 
 con.close()
 
+from datetime import datetime
+time=datetime.now()
+file='CLC_'+tgt_t+'_'+time.strftime("%j")+time.strftime("%Y")+time.strftime("%H")+time.strftime("%M")+time.strftime("%S")+'.csv'
+file_path='C:\\'+file
+
+log=str(time)+'\n \n'+'Source: '+src_t+'\n \n'+'Target: '+tgt_d+'.'+tgt_s+'.'+tgt_t+'\n \n'+'Test query: \n'+q
+
+log_path='C:\\log_'+tgt_t+'_'+time.strftime("%j")+time.strftime("%Y")+time.strftime("%H")+time.strftime("%M")+time.strftime("%S")+'.txt'
+f=open(log_path,'w')
+f.write(log)
+f.close()
+print('Log file saved in '+log_path)
+
 if df.shape[0]==0:
     print('Column level check passed')
 else:
     print(str(df.shape[0])+' unmatched records')
+    out=input('Do you want the unmatched records to be stored locally (y/n): ')
+    if out=='y' or out=='Y':
+        df.to_csv(file_path)
+        print('File saved successfully '+file_path)
+    else:
+        print('')
